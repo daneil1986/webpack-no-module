65008d26ff76967848e07c5a9f82e6b8c234ec5d
lixianliang <lixianliang@kingsoft.com>
2018-04-16 16:50:41 +0800

    实现

--- index.js
+++ index.js
@@ -1,14 +1,58 @@
 
+var _ = require('lodash');
+var path = require('path');
+var fs = require('fs');
 
 function webpackNoModules (options) {
-
+    this.options = _.extend({
+        target: path.join(__dirname, '../../static'),
+        dest: path.join(__dirname, '../../dist')
+      }, options);
 }
 
 webpackNoModules.prototype.apply = function (compiler) {
-    compiler.plugin('emit', function (compilation, callback) {
-        console.log('_________ dir ___________', __dirname);
+    compiler.plugin('emit', (compilation, callback) => {
+        let target = this.options.target;
+        let dest = this.options.dest;
+        let baseName = path.basename(target);
+
+        if (!fs.existsSync(dest)) {
+            fs.mkdirSync(dest);
+        }
+        let destDir = this.options.dest + '/' + baseName;
+
+        copyFiles(target, destDir);
         callback();
     });
 }
 
+function copyFiles (target, dest) {
+    if (!fs.existsSync(dest)) {
+        fs.mkdirSync(dest);
+    }
+    if (fs.existsSync(target)) {
+        let files = fs.readdirSync(target);
+
+        files.forEach((file) => {
+            let t = target + '/' + file;
+            let d = dest + '/' + file;
+
+            if (fs.statSync(t).isDirectory()) {
+                if (!fs.existsSync(d)) {
+                    fs.mkdirSync(d);
+                }
+                copyFiles(t, d);
+            } else {
+                if (file.indexOf('.') > 0) {
+                    fs.copyFileSync(t, d);
+                }
+                
+            }
+        });
+    } else {
+        console.log(' file no exist');
+    }
+}
+
 module.exports = webpackNoModules;
+
--- test.js
+++ test.js
@@ -1,3 +1,10 @@
 
+const path = require('path');
+const fs = require('fs');
+console.log(__dirname);
 
-console.log(_dirname);
+const workFile = path.resolve();
+
+let files = fs.readdirSync('/');
+
+fs.copyFileSync(workFile + '/index.js', workFile + '/bak/index2.js');
